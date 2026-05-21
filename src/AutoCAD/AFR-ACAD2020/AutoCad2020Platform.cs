4732ae4b77cd7ae2594787c12ec0f8a9a765d6be
splrad <alearner@splrad.com>
2026-05-21 21:17:57 +0700

    refactor(cad-platform): 统一代码布局并移除未使用的图案数组
    - 将版本标识属性（BrandName/VersionName等）移至类首，移除冗余注释
    - 格式化 NativeHookTarget.Export 调用为多行参数，提升可读性
    - 移除 AutoCad2027Platform 中未使用的 MainDispatcherPattern 和
    ParallelDispatcherPattern 静态数组

--- src/AutoCAD/AFR-ACAD2020/AutoCad2020Platform.cs
+++ src/AutoCAD/AFR-ACAD2020/AutoCad2020Platform.cs
@@ -9,48 +9,71 @@ namespace AFR;
 /// </summary>
 internal sealed class AutoCad2020Platform : ICadPlatform, INativeFontHookExportsProvider
 {
+    public string BrandName => "AutoCAD";
+    public string VersionName => "2020";
+    public string AppName => "AFR-ACAD2020";
+    public string DisplayName => "AutoCAD 2020";
+    public string RegistryBasePath => @"Software\Autodesk\AutoCAD\R23.1";
+    public string RegistryKeyPattern => @"^ACAD-[A-Za-z0-9]+:[A-Za-z0-9]+$";
+    public string AcDbDllName => "acdb23.dll";
+    public bool SupportsNativeFontHooks => true;
+
     public string AcGiTextStyleLoadStyleRecExport => "?loadStyleRec@AcGiTextStyle@@UEBAHPEAVAcDbDatabase@@@Z";
-
     public string AcGiTextStyleStyleNameExport => "?styleName@AcGiTextStyle@@UEBAPEB_WXZ";
-
     public string AcGiTextStyleFileNameExport => "?fileName@AcGiTextStyle@@UEBAPEB_WXZ";
-
     public string AcGiTextStyleBigFontFileNameExport => "?bigFontFileName@AcGiTextStyle@@UEBAPEB_WXZ";
-
     public string AcGiTextStyleIsVerticalExport => "?isVertical@AcGiTextStyle@@UEBA_NXZ";
-
     public string AcGiTextStyleSetVerticalExport => "?setVertical@AcGiTextStyle@@UEAAX_N@Z";
-
     public string AcGiTextStyleSetFontExport => "?setFont@AcGiTextStyle@@UEAA?AW4ErrorStatus@Acad@@PEB_W_N1W4Charset@@W4FontPitch@FontUtils@PAL@AutoCAD@Autodesk@@W4FontFamily@6789@@Z";
-
     public string AcGiTextStyleSetFileNameExport => "?setFileName@AcGiTextStyle@@UEAAXPEB_W@Z";
-
     public string AcGiTextStyleSetBigFontFileNameExport => "?setBigFontFileName@AcGiTextStyle@@UEAAXPEB_W@Z";
-
     public string AcGiTextStyleFileNameCtorExport => "??0AcGiTextStyle@@QEAA@PEB_W0NNNN_N111110@Z";
-
     public string AcDbMTextExplodeFragmentsExport => "?explodeFragments@AcDbMText@@QEBAXP6AHPEAUAcDbMTextFragment@@PEAX@Z1PEAVAcGiWorldDraw@@@Z";
-
     public string LdFileExport => "?ldfile@@YAHPEB_WHPEAVAcDbDatabase@@PEAVAcFontDescription@@@Z";
-
     public uint? LdFileRva => 0x47BF0;
 
     public NativeFontHookProfile NativeFontHookProfile
         => new(
-            NativeHookTarget.Export("AcGiTextStyle::loadStyleRec", AcGiTextStyleLoadStyleRecExport, 0x4B708, [0x40, 0x53, 0x48, 0x83, 0xEC, 0x20, 0x48, 0x8B, 0xDA, 0xFF, 0x15, 0xD1, 0x95, 0xCA, 0x00, 0x48], maxPrologueSize: 64),
-            NativeHookTarget.Export("AcGiTextStyle::setFont", AcGiTextStyleSetFontExport, 0x815C0, [0x48, 0x89, 0x5C, 0x24, 0x08, 0x48, 0x89, 0x74, 0x24, 0x10, 0x57, 0x48, 0x83, 0xEC, 0x40, 0x41], maxPrologueSize: 64),
-            NativeHookTarget.Export("AcGiTextStyle::setFileName", AcGiTextStyleSetFileNameExport, 0x816D8, [0x40, 0x53, 0x48, 0x83, 0xEC, 0x20, 0x48, 0x8B, 0xDA, 0xFF, 0x15, 0x31, 0x37, 0xC7, 0x00, 0x48], maxPrologueSize: 64),
-            NativeHookTarget.Export("AcGiTextStyle::setBigFontFileName", AcGiTextStyleSetBigFontFileNameExport, 0x81598, [0x40, 0x53, 0x48, 0x83, 0xEC, 0x20, 0x48, 0x8B, 0xDA, 0xFF, 0x15, 0x71, 0x38, 0xC7, 0x00, 0x48], maxPrologueSize: 64),
-            NativeHookTarget.Export("AcGiTextStyle::AcGiTextStyle(font,bigFont)", AcGiTextStyleFileNameCtorExport, 0xAF9A9C, [0x48, 0x8B, 0xC4, 0x48, 0x89, 0x48, 0x08, 0x57, 0x48, 0x81, 0xEC, 0x90, 0x00, 0x00, 0x00, 0x48], maxPrologueSize: 96),
-            NativeHookTarget.Export("AcDbMText::explodeFragments", AcDbMTextExplodeFragmentsExport, 0xA83A44, [0x48, 0x8B, 0xC1, 0x33, 0xC9, 0x48, 0x85, 0xC0, 0x74, 0x04, 0x48, 0x8B, 0x48, 0x10, 0xE9, 0x6D], maxPrologueSize: 64),
-            NativeHookTarget.Export("ldfile", LdFileExport, 0x47BF0, [0x48, 0x8B, 0xC4, 0x55, 0x56, 0x57, 0x41, 0x54, 0x41, 0x55, 0x41, 0x56, 0x41, 0x57, 0x48, 0x8D], maxPrologueSize: 64));
-
-    public string BrandName => "AutoCAD";
-    public string VersionName => "2020";
-    public string AppName => "AFR-ACAD2020";                    // 注册表中的应用名称
-    public string DisplayName => "AutoCAD 2020";
-    public string RegistryBasePath => @"Software\Autodesk\AutoCAD\R23.1";  // AutoCAD 2020 的注册表基路径
-    public string RegistryKeyPattern => @"^ACAD-[A-Za-z0-9]+:[A-Za-z0-9]+$"; // 匹配配置文件子键的正则
-    public string AcDbDllName => "acdb23.dll";                  // AutoCAD 2020 的数据库 DLL
-    public bool SupportsNativeFontHooks => true;
+            NativeHookTarget.Export(
+                "AcGiTextStyle::loadStyleRec",
+                AcGiTextStyleLoadStyleRecExport,
+                0x4B708,
+                [0x40, 0x53, 0x48, 0x83, 0xEC, 0x20, 0x48, 0x8B, 0xDA, 0xFF, 0x15, 0xD1, 0x95, 0xCA, 0x00, 0x48],
+                maxPrologueSize: 64),
+            NativeHookTarget.Export(
+                "AcGiTextStyle::setFont",
+                AcGiTextStyleSetFontExport,
+                0x815C0,
+                [0x48, 0x89, 0x5C, 0x24, 0x08, 0x48, 0x89, 0x74, 0x24, 0x10, 0x57, 0x48, 0x83, 0xEC, 0x40, 0x41],
+                maxPrologueSize: 64),
+            NativeHookTarget.Export(
+                "AcGiTextStyle::setFileName",
+                AcGiTextStyleSetFileNameExport,
+                0x816D8,
+                [0x40, 0x53, 0x48, 0x83, 0xEC, 0x20, 0x48, 0x8B, 0xDA, 0xFF, 0x15, 0x31, 0x37, 0xC7, 0x00, 0x48],
+                maxPrologueSize: 64),
+            NativeHookTarget.Export(
+                "AcGiTextStyle::setBigFontFileName",
+                AcGiTextStyleSetBigFontFileNameExport,
+                0x81598,
+                [0x40, 0x53, 0x48, 0x83, 0xEC, 0x20, 0x48, 0x8B, 0xDA, 0xFF, 0x15, 0x71, 0x38, 0xC7, 0x00, 0x48],
+                maxPrologueSize: 64),
+            NativeHookTarget.Export(
+                "AcGiTextStyle::AcGiTextStyle(font,bigFont)",
+                AcGiTextStyleFileNameCtorExport,
+                0xAF9A9C,
+                [0x48, 0x8B, 0xC4, 0x48, 0x89, 0x48, 0x08, 0x57, 0x48, 0x81, 0xEC, 0x90, 0x00, 0x00, 0x00, 0x48],
+                maxPrologueSize: 96),
+            NativeHookTarget.Export(
+                "AcDbMText::explodeFragments",
+                AcDbMTextExplodeFragmentsExport,
+                0xA83A44,
+                [0x48, 0x8B, 0xC1, 0x33, 0xC9, 0x48, 0x85, 0xC0, 0x74, 0x04, 0x48, 0x8B, 0x48, 0x10, 0xE9, 0x6D],
+                maxPrologueSize: 64),
+            NativeHookTarget.Export(
+                "ldfile",
+                LdFileExport,
+                0x47BF0,
+                [0x48, 0x8B, 0xC4, 0x55, 0x56, 0x57, 0x41, 0x54, 0x41, 0x55, 0x41, 0x56, 0x41, 0x57, 0x48, 0x8D],
+                maxPrologueSize: 64));
 }
--- src/AutoCAD/AFR-ACAD2021/AutoCad2021Platform.cs
+++ src/AutoCAD/AFR-ACAD2021/AutoCad2021Platform.cs
@@ -9,48 +9,71 @@ namespace AFR;
 /// </summary>
 internal sealed class AutoCad2021Platform : ICadPlatform, INativeFontHookExportsProvider
 {
+    public string BrandName => "AutoCAD";
+    public string VersionName => "2021";
+    public string AppName => "AFR-ACAD2021";
+    public string DisplayName => "AutoCAD 2021";
+    public string RegistryBasePath => @"Software\Autodesk\AutoCAD\R24.0";
+    public string RegistryKeyPattern => @"^ACAD-[A-Za-z0-9]+:[A-Za-z0-9]+$";
+    public string AcDbDllName => "acdb24.dll";
+    public bool SupportsNativeFontHooks => true;
+
     public string AcGiTextStyleLoadStyleRecExport => "?loadStyleRec@AcGiTextStyle@@UEBAHPEAVAcDbDatabase@@@Z";
-
     public string AcGiTextStyleStyleNameExport => "?styleName@AcGiTextStyle@@UEBAPEB_WXZ";
-
     public string AcGiTextStyleFileNameExport => "?fileName@AcGiTextStyle@@UEBAPEB_WXZ";
-
     public string AcGiTextStyleBigFontFileNameExport => "?bigFontFileName@AcGiTextStyle@@UEBAPEB_WXZ";
-
     public string AcGiTextStyleIsVerticalExport => "?isVertical@AcGiTextStyle@@UEBA_NXZ";
-
     public string AcGiTextStyleSetVerticalExport => "?setVertical@AcGiTextStyle@@UEAAX_N@Z";
-
     public string AcGiTextStyleSetFontExport => "?setFont@AcGiTextStyle@@UEAA?AW4ErrorStatus@Acad@@PEB_W_N1W4Charset@@W4FontPitch@FontUtils@PAL@AutoCAD@Autodesk@@W4FontFamily@6789@@Z";
-
     public string AcGiTextStyleSetFileNameExport => "?setFileName@AcGiTextStyle@@UEAAXPEB_W@Z";
-
     public string AcGiTextStyleSetBigFontFileNameExport => "?setBigFontFileName@AcGiTextStyle@@UEAAXPEB_W@Z";
-
     public string AcGiTextStyleFileNameCtorExport => "??0AcGiTextStyle@@QEAA@PEB_W0NNNN_N111110@Z";
-
     public string AcDbMTextExplodeFragmentsExport => "?explodeFragments@AcDbMText@@QEBAXP6AHPEAUAcDbMTextFragment@@PEAX@Z1PEAVAcGiWorldDraw@@@Z";
-
     public string LdFileExport => "?ldfile@@YAHPEB_WHPEAVAcDbDatabase@@PEAVAcFontDescription@@@Z";
-
     public uint? LdFileRva => 0x527BC;
 
     public NativeFontHookProfile NativeFontHookProfile
         => new(
-            NativeHookTarget.Export("AcGiTextStyle::loadStyleRec", AcGiTextStyleLoadStyleRecExport, 0x4EC4C, [0x40, 0x53, 0x48, 0x83, 0xEC, 0x20, 0x48, 0x8B, 0xDA, 0xFF, 0x15, 0x8D, 0xAF, 0xAD, 0x00, 0x48], maxPrologueSize: 64),
-            NativeHookTarget.Export("AcGiTextStyle::setFont", AcGiTextStyleSetFontExport, 0x538F0, [0x48, 0x89, 0x5C, 0x24, 0x08, 0x48, 0x89, 0x74, 0x24, 0x10, 0x57, 0x48, 0x83, 0xEC, 0x40, 0x41], maxPrologueSize: 64),
-            NativeHookTarget.Export("AcGiTextStyle::setFileName", AcGiTextStyleSetFileNameExport, 0x53A0C, [0x40, 0x53, 0x48, 0x83, 0xEC, 0x20, 0x48, 0x8B, 0xDA, 0xFF, 0x15, 0xFD, 0x62, 0xAD, 0x00, 0x48], maxPrologueSize: 64),
-            NativeHookTarget.Export("AcGiTextStyle::setBigFontFileName", AcGiTextStyleSetBigFontFileNameExport, 0x538C8, [0x40, 0x53, 0x48, 0x83, 0xEC, 0x20, 0x48, 0x8B, 0xDA, 0xFF, 0x15, 0x41, 0x64, 0xAD, 0x00, 0x48], maxPrologueSize: 64),
-            NativeHookTarget.Export("AcGiTextStyle::AcGiTextStyle(font,bigFont)", AcGiTextStyleFileNameCtorExport, 0xAB25C4, [0x48, 0x8B, 0xC4, 0x48, 0x89, 0x48, 0x08, 0x57, 0x48, 0x81, 0xEC, 0x90, 0x00, 0x00, 0x00, 0x48], maxPrologueSize: 96),
-            NativeHookTarget.Export("AcDbMText::explodeFragments", AcDbMTextExplodeFragmentsExport, 0xA3DA9C, [0x48, 0x8B, 0xC1, 0x33, 0xC9, 0x48, 0x85, 0xC0, 0x74, 0x04, 0x48, 0x8B, 0x48, 0x10, 0xE9, 0x25], maxPrologueSize: 64),
-            NativeHookTarget.Export("ldfile", LdFileExport, 0x527BC, [0x40, 0x55, 0x53, 0x56, 0x57, 0x41, 0x54, 0x41, 0x55, 0x41, 0x56, 0x41, 0x57, 0x48, 0x8D, 0xAC], maxPrologueSize: 64));
-
-    public string BrandName => "AutoCAD";
-    public string VersionName => "2021";
-    public string AppName => "AFR-ACAD2021";                    // 注册表中的应用名称
-    public string DisplayName => "AutoCAD 2021";
-    public string RegistryBasePath => @"Software\Autodesk\AutoCAD\R24.0";  // AutoCAD 2021 的注册表基路径
-    public string RegistryKeyPattern => @"^ACAD-[A-Za-z0-9]+:[A-Za-z0-9]+$"; // 匹配配置文件子键的正则
-    public string AcDbDllName => "acdb24.dll";                  // AutoCAD 2021 的数据库 DLL
-    public bool SupportsNativeFontHooks => true;
+            NativeHookTarget.Export(
+                "AcGiTextStyle::loadStyleRec",
+                AcGiTextStyleLoadStyleRecExport,
+                0x4EC4C,
+                [0x40, 0x53, 0x48, 0x83, 0xEC, 0x20, 0x48, 0x8B, 0xDA, 0xFF, 0x15, 0x8D, 0xAF, 0xAD, 0x00, 0x48],
+                maxPrologueSize: 64),
+            NativeHookTarget.Export(
+                "AcGiTextStyle::setFont",
+                AcGiTextStyleSetFontExport,
+                0x538F0,
+                [0x48, 0x89, 0x5C, 0x24, 0x08, 0x48, 0x89, 0x74, 0x24, 0x10, 0x57, 0x48, 0x83, 0xEC, 0x40, 0x41],
+                maxPrologueSize: 64),
+            NativeHookTarget.Export(
+                "AcGiTextStyle::setFileName",
+                AcGiTextStyleSetFileNameExport,
+                0x53A0C,
+                [0x40, 0x53, 0x48, 0x83, 0xEC, 0x20, 0x48, 0x8B, 0xDA, 0xFF, 0x15, 0xFD, 0x62, 0xAD, 0x00, 0x48],
+                maxPrologueSize: 64),
+            NativeHookTarget.Export(
+                "AcGiTextStyle::setBigFontFileName",
+                AcGiTextStyleSetBigFontFileNameExport,
+                0x538C8,
+                [0x40, 0x53, 0x48, 0x83, 0xEC, 0x20, 0x48, 0x8B, 0xDA, 0xFF, 0x15, 0x41, 0x64, 0xAD, 0x00, 0x48],
+                maxPrologueSize: 64),
+            NativeHookTarget.Export(
+                "AcGiTextStyle::AcGiTextStyle(font,bigFont)",
+                AcGiTextStyleFileNameCtorExport,
+                0xAB25C4,
+                [0x48, 0x8B, 0xC4, 0x48, 0x89, 0x48, 0x08, 0x57, 0x48, 0x81, 0xEC, 0x90, 0x00, 0x00, 0x00, 0x48],
+                maxPrologueSize: 96),
+            NativeHookTarget.Export(
+                "AcDbMText::explodeFragments",
+                AcDbMTextExplodeFragmentsExport,
+                0xA3DA9C,
+                [0x48, 0x8B, 0xC1, 0x33, 0xC9, 0x48, 0x85, 0xC0, 0x74, 0x04, 0x48, 0x8B, 0x48, 0x10, 0xE9, 0x25],
+                maxPrologueSize: 64),
+            NativeHookTarget.Export(
+                "ldfile",
+                LdFileExport,
+                0x527BC,
+                [0x40, 0x55, 0x53, 0x56, 0x57, 0x41, 0x54, 0x41, 0x55, 0x41, 0x56, 0x41, 0x57, 0x48, 0x8D, 0xAC],
+                maxPrologueSize: 64));
 }
